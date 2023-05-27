b95c64d18f5d158db819908f3df526840410d71c
Morten Bjerg Gregersen <759680+MortenGregersen@users.noreply.github.com>
2023-05-27 22:41:46 +0200

    Initial Commit

---
+++ MissingAccentColor/MissingAccentColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MissingAccentColorApp.swift
+//  MissingAccentColor
+//
+//  Created by Morten Bjerg Gregersen on 27/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct MissingAccentColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
